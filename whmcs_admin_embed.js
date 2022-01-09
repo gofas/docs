2b919df6d7c9982ba39c9d64d66ebf7f1b3a5188
mauriciogofas <mauricio@gofas.net>
2022-01-09 18:05:16 -0200

    iframe test

--- whmcs_admin_embed.js
+++ whmcs_admin_embed.js
@@ -3,3 +3,9 @@
  * Script para entrega de atualizações
  * 
  */
+
+ $(document).ready(function () {
+     var gjpifrm = document.getElementById("gjpifrm")
+     var iframe = gjpifrm.load("https://gofas.net/whmcs/juno-cartao/");
+    setTimeout(function(){ gjpifrm.innerHTML = iframe; }, 3000);
+})
