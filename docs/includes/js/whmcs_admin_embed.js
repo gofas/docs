33fdf89b7de9ca4bd2cb6ce02e35cdbc73de2cfa
mauriciogofas <mauricio@gofas.net>
2022-01-09 18:40:47 -0200

    Up

--- docs/includes/js/whmcs_admin_embed.js
+++ docs/includes/js/whmcs_admin_embed.js
@@ -5,14 +5,7 @@
  */
 
 $(document).ready(function () {
-     console.log("Hey")
-    
-        var ifrm = document.createElement("gjpifrm");
-        ifrm.setAttribute("src", "https://gofas.net/whmcs/juno-cartao/");
-        ifrm.style.width = "640px";
-        ifrm.style.height = "480px";
-        document.body.appendChild(ifrm);
-  
-    // $("gjpifrm").load("https://gofas.net/whmcs/juno-cartao/");
-   // setTimeout(function(){ gjpifrm.innerHTML = iframe; }, 3000);
+    console.log("Hey")
+    var tooltip = document.getElementById("gjpifrm");
+    gjpifrm.innerHTML = '<iframe id="myFrame" src="https://gofas.net/whmcs/juno-cartao/"></iframe>';
 })
