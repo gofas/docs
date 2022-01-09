9eb9b3112e6eb581778c2a96c81011185aba1040
mauriciogofas <mauricio@gofas.net>
2022-01-09 19:35:28 -0200

    Up

--- docs/includes/js/whmcs_admin_embed.js
+++ docs/includes/js/whmcs_admin_embed.js
@@ -3,9 +3,3 @@
  * Script para entrega de atualizações
  * 
  */
-
-$(document).ready(function () {
-    console.log("Hey")
-    var tooltip = document.getElementById("gjpifrm");
-    gjpifrm.innerHTML = '<iframe id="myFrame" src="https://gofas.net/whmcs/embeds/"></iframe>';
-})
