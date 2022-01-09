4ac6b7f7dbc301d6b9ba8f0d657f60855b00ffe6
mauriciogofas <mauricio@gofas.net>
2022-01-09 19:30:07 -0200

    Up

--- docs/includes/js/whmcs_admin_embed.js
+++ docs/includes/js/whmcs_admin_embed.js
@@ -7,5 +7,5 @@
 $(document).ready(function () {
     console.log("Hey")
     var tooltip = document.getElementById("gjpifrm");
-    gjpifrm.innerHTML = '<iframe id="myFrame" src="https://gofas.net/whmcs/juno-cartao/"></iframe>';
+    gjpifrm.innerHTML = '<iframe id="myFrame" src="https://gofas.net/whmcs/embeds/"></iframe>';
 })
